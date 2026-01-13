d648b2af2feeb58a3483dc8eac8e5a264d48615c
rodrigogramitto <rgramitto@gmail.com>
2026-01-13 16:54:12 -0500

    Write boilerplate functions

--- src/VMTranslator/code_writer/code_writer.py
+++ src/VMTranslator/code_writer/code_writer.py
@@ -3,3 +3,12 @@
 class CodeWriter:
     def __init__(self):
         return
+
+    def writeArithmetic(self, command):
+        return
+
+    def writePushPop(self, command, segment, index):
+        return
+
+    def close(self):
+        return
--- src/VMTranslator/parser/parse.py
+++ src/VMTranslator/parser/parse.py
@@ -1,5 +1,25 @@
-
+from pathlib import Path
+from collections import deque
+import re
 
 class Parser:
   def __init__(self):
+    return
+
+  def hasMoreLines(self):
+    #implement logic here
+    return False
+
+  def advance(self):
+    #implement logic here
+    return
+
+  def commandType(self):
+    # implement logic here
+    return
+
+  def arg1(self):
+    return
+
+  def arg2(self):
     return
